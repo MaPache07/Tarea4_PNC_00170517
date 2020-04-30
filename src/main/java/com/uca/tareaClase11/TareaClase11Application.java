45aa9952a99f9bf3911df95933bfa462a3792e9c
MaPache07 <00170517@uca.edu.sv>
2020-04-30 10:44:17 -0600

    Se ha inicializado el projeccto

---
+++ src/main/java/com/uca/tareaClase11/TareaClase11Application.java
@@ -0,0 +1,13 @@
+package com.uca.tareaClase11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaClase11Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaClase11Application.class, args);
+	}
+
+}
